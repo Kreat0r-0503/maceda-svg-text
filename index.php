e70011be5478387f987574fd79e8709c6c4d0754
Ec da KreatOr <64685270+Kreat0r-0503@users.noreply.github.com>
2020-11-11 16:28:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="em">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> SVG TEXT PATH </title>
+	<link rel="stylesheet" href="style.css">
+	
+</head>
+<body>
+    <svg width="900" height="780" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+        <!-- Created with SVG-edit - https://github.com/SVG-Edit/svgedit-->
+        <title>cat</title>
+        <g class="layer">
+         <title>Layer 1</title>
+         <text fill="#000" font-family="serif" font-size="24" id="svg_2" stroke="97C1A9" stroke opacity= "0.7" stroke-width="1" text-anchor="middle" transform="rotate(-0.252504 410.172 230)" x="410" xml:space="preserve" y="238">Beautiful</text>
+         <text fill="#000" font-family="serif" font-size="24" id="svg_4" stroke="97C1A9" stroke opacity= "0.7" stroke-width="5" text-anchor="middle" transform="rotate(40.2175 334.5 401)" x="335" xml:space="preserve" y="409">Amazing</text>
+         <text fill="#000" font-family="serif" font-size="24" id="svg_5" stroke="97C1A9"stroke opacity= "0.7"  stroke-width="1" text-anchor="middle" transform="rotate(-48.6522 330.172 267)" x="330" xml:space="preserve" y="275">Joyful</text>
+         <text fill="#000" font-family="serif" font-size="24" id="svg_6" stroke="97C1A9" stroke opacity= "0.7" stroke-width="1" text-anchor="middle" transform="rotate(47.153 494.5 259)" x="494" xml:space="preserve" y="267">Happy</text>
+         <text fill="#000" font-family="serif" font-size="24" id="svg_7" stroke="97C1A9" stroke opacity= "0.7" stroke-width="1" text-anchor="middle" transform="rotate(-41.3785 492.164 403)" x="492" xml:space="preserve" y="411">Optimist</text>
+         <text fill="#000" font-family="serif" font-size="24" id="svg_8" stroke="97C1A9" stroke opacity= "0.7" stroke-width="1" text-anchor="middle" x="412" xml:space="preserve" y="445">Family</text>
+         <text fill="#000" font-family="serif" font-size="24" id="svg_9" stroke="97C1A9" stroke opacity= "0.7" stroke-width="1" text-anchor="middle" transform="rotate(82.6623 524.172 328)" x="524" xml:space="preserve" y="336">Hope</text>
+         <text fill="#000" font-family="serif" font-size="24" id="svg_10" stroke="97C1A9" stroke opacity= "0.7" stroke-width="1" text-anchor="middle" transform="rotate(-85.5311 298.18 331)" x="298" xml:space="preserve" y="339">Love</text>
+        </g>
+       </svg>
